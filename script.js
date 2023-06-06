de3e088691495fb0f5d856f6638e9f097d505497
PromiMojumder38 <74852911+PromiMojumder38@users.noreply.github.com>
2023-06-06 16:34:09 +0600

    mobile responsive: complete

--- script.js
+++ script.js
@@ -4,20 +4,42 @@ eduContainers.forEach((container, index) => {
   container.classList.add('slide-in');
   container.style.animationDelay = `${index * 2}s`;
 });
-var menuToggle = document.getElementById('check');
+
+
+
+var menuToggle = document.getElementById('checkbtn');
 var list = document.querySelector('ul');
-var menuIcon = document.querySelector('.checkbtn');
 var closeIcon = document.querySelector('.closebtn');
 
 menuToggle.addEventListener('change', function() {
-  list.style.left = '0';
-    menuIcon.style.display = 'none';
+   list.style.left = '0';
     closeIcon.style.display = 'block';
+    menuToggle.style.display = 'none';
 
 });
 
 closeIcon.addEventListener('click', function() {
+  menuToggle.checked = false;
   list.style.left = '-100%';
-  menuIcon.style.display = 'block';
   closeIcon.style.display = 'none';
+  menuToggle.style.display = 'block';
 });
+
+var listItems = document.querySelector('ul li a');
+
+
+  listItems.addEventListener('click', function() {
+    list.style.display ='none';
+    closeIcon.style.display = 'none';
+    menuToggle.style.display = 'block';
+  });
+  var navbar = document.querySelector('.navbar');
+
+navbar.addEventListener('touchstart', function() {
+  navbar.classList.add('touched');
+});
+
+navbar.addEventListener('touchend', function() {
+  navbar.classList.remove('touched');
+});
+
