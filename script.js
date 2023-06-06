b0a4088d8d81b23ea47212ca38b92f8d012ad72b
PromiMojumder38 <74852911+PromiMojumder38@users.noreply.github.com>
2023-06-06 15:20:57 +0600

    image display solved for mobile and set screen for desktop

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ eduContainers.forEach((container, index) => {
   container.classList.add('slide-in');
   container.style.animationDelay = `${index * 2}s`;
 });
+var menuToggle = document.getElementById('check');
+var list = document.querySelector('ul');
+var menuIcon = document.querySelector('.checkbtn');
+var closeIcon = document.querySelector('.closebtn');
+
+menuToggle.addEventListener('change', function() {
+  list.style.left = '0';
+    menuIcon.style.display = 'none';
+    closeIcon.style.display = 'block';
+
+});
+
+closeIcon.addEventListener('click', function() {
+  list.style.left = '-100%';
+  menuIcon.style.display = 'block';
+  closeIcon.style.display = 'none';
+});
