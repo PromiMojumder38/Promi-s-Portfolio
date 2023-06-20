a8414a2d606981749297295eb233e70d81dee467
PromiMojumder38 <74852911+PromiMojumder38@users.noreply.github.com>
2023-06-20 22:03:33 +0600

    fix: mobile responsive

--- script.js
+++ script.js
@@ -9,12 +9,28 @@ eduContainers.forEach((container, index) => {
 
 var menuToggle = document.getElementById('checkbtn');
 var list = document.querySelector('ul');
+var closeIcon = document.querySelector('.closebtn');
 
 menuToggle.addEventListener('change', function() {
    list.style.left = '0';
+    closeIcon.style.display = 'block';
+    menuToggle.style.display = 'none';
+
 });
 
 closeIcon.addEventListener('click', function() {
   menuToggle.checked = false;
   list.style.left = '-100%';
+  closeIcon.style.display = 'none';
+  menuToggle.style.display = 'block';
 });
+
+var listItems = document.querySelector('ul li a');
+
+
+  listItems.addEventListener('click', function() {
+    list.style.display ='none';
+    closeIcon.style.display = 'none';
+    menuToggle.style.display = 'block';
+  });
+  
